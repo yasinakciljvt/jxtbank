98a3d7073714a10e41d87a5130ea6b251714c893
yasinakciljvt <akcil.yasiin@gmail.com>
2026-01-06 23:54:19 +0300

    committed to database

---
+++ src/main/java/com/jxt/jxtbank/JxtbankApplication.java
@@ -0,0 +1,13 @@
+package com.jxt.jxtbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxtbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JxtbankApplication.class, args);
+	}
+
+}
